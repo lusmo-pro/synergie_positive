903287be1c702bb3fe752438d7ccdcfb5b62d5b6
Ehouarn-Nouelig Sioly <eh.dev.sioly@gmail.com>
2025-10-22 18:55:59 +0200

    Contact: Create Contact Section

---
+++ frontend/src/Components/sections/Contact.tsx
@@ -0,0 +1,22 @@
+// import TeamGallery from "@/Components/assets/TeamGallery/TeamGallery";
+
+import "./Contact.scss";
+
+export default function SectionContact() {
+  return (
+    <section id="Contact" className="section-contact">
+      <div className="section-contact__header">
+        <h2 className="section-contact__title">Nous Contacter</h2>
+        <p className="section-contact__description">
+          Vous avez une question, une idée ou l’envie de collaborer avec nous ?
+          Cette section est faite pour vous. Nous serons ravis d’échanger,
+          d’écouter vos suggestions ou de vous accompagner dans vos démarches.
+          Que ce soit pour rejoindre nos projets, proposer une initiative ou
+          simplement en savoir plus sur notre association, n’hésitez pas à nous
+          écrire. Chaque message compte et contribue à faire grandir notre
+          communauté solidaire.
+        </p>
+      </div>
+    </section>
+  );
+}
